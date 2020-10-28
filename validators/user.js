b27dfc495d780dff24e92e00cdfd1bd42379a205
akshayx11 <akshayx11@gmail.com>
2020-10-28 18:20:46 +0530

    minor css changes

--- validators/user.js
+++ validators/user.js
@@ -3,16 +3,16 @@ const Joi = require("joi");
 exports.userSignupValidate = Joi.object({
     title: Joi.string().required(),
     firstName: Joi.string().required(),
-    middleName: Joi.string(),
+    middleName: Joi.string().allow(""),
     lastName: Joi.string().required(),
     email: Joi.string().required(),
     password: Joi.string().required(),
     dpURL: Joi.string(),
     mobileno: Joi.string(),
     dob: Joi.number().required(),
-    city : Joi.string(),
-	state : Joi.string(),
-	country : Joi.string(),
+    city : Joi.string().required(),
+	state : Joi.string().required(),
+	country : Joi.string().required(),
     gender: Joi.string().required()
 });
 
